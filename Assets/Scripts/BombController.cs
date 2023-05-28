87ff787e29dca0d76a0e9b714ce2c0ff7a1cf88e
Rinery <91065334+Rinery@users.noreply.github.com>
2023-05-29 02:36:20 +0300

    Destructibles
    Created destructible tiles feature

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BombController : MonoBehaviour
 {
@@ -17,6 +18,10 @@ public class BombController : MonoBehaviour
    public LayerMask explosionLayerMask;
    public float explosionDuration = 1f;
    public int explosionRadius = 1;
+
+   [Header("Destructible")] 
+   public Tilemap destructibleTiles;
+   public Destructible destructiblePrefab;
 
    private void OnEnable()
    {
@@ -78,6 +83,7 @@ public class BombController : MonoBehaviour
 
       if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
       {
+         ClearDestructible(position);
          return;
       }
       
@@ -87,5 +93,17 @@ public class BombController : MonoBehaviour
       explosion.DestroyAfter(explosionDuration);
 
       Explode(position, direction, length - 1);
+   }
+
+   private void ClearDestructible(Vector2 position)
+   {
+      Vector3Int cell = destructibleTiles.WorldToCell(position);
+      TileBase tile = destructibleTiles.GetTile(cell);
+
+      if (tile != null)
+      {
+         Instantiate(destructiblePrefab, position, Quaternion.identity);
+         destructibleTiles.SetTile(cell, null);
+      }
    }
 }
