136810a825a4473e27bff32c8662c271ff2d4d8f
Rinery <91065334+Rinery@users.noreply.github.com>
2023-05-29 03:06:33 +0300

    Bonus Items Pickups
    Created bonus items feature

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public enum ItemType
+    {
+        BlastRadius,
+        ExtraBomb,
+        SpeedIncrease,
+    }
+
+    public ItemType type;
+
+    private void OnItemPickup(GameObject player)
+    {
+        switch (type)
+        {
+            case ItemType.BlastRadius:
+                player.GetComponent<BombController>().explosionRadius++;
+                break;
+            
+            case ItemType.ExtraBomb:
+                player.GetComponent<BombController>().AddBomb();
+                break;
+            
+            case ItemType.SpeedIncrease:
+                player.GetComponent<MovementController>().speed++;
+                break;
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnItemPickup(other.gameObject);
+        }
+    }
+}
